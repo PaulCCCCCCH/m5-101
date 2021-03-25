1e140644b3142b01e2b65ebfe6c10a3864d0d576
paulcccccch <576140292@qq.com>
2021-03-25 18:17:53 +0800

    Deployed 28131f5 with MkDocs version: 1.1.2

--- content/solutions/1_equation-solver.py
+++ content/solutions/1_equation-solver.py
@@ -15,10 +15,12 @@ def solve_linear(a, b):
     """
     求解方程　ax + b = 0
     """
-    ######################
-    ### Your code here ###
-    ######################
-    return None
+    if a == 0 and b == 0:
+        return None
+    elif a == 0:
+        return []
+    else:
+        return [-b / a]
 
 #################
 ### Section 2 ###
@@ -27,22 +29,33 @@ def solve_with_formula(a, b, c):
     """
     求解方程　ax^2 + bx + c = 0
     """
-    ######################
-    ### Your code here ###
-    ######################
-    return None
+    if a == 0:
+        return solve_linear(b, c)
+
+    delta = b ** 2 - 4 * a * c
+    if delta < 0:
+        return []
+    elif delta == 0:
+        return [-b / (2*a)]
+    else:
+        delta_sqrt = delta ** 0.5
+        return [(-b + delta_sqrt) / (2*a), (-b - delta_sqrt) / (2*a)]
 
 #################
 ### Section 3 ###
 #################
+def d(f):
+    return lambda x: (f(x + 1e-6) - f(x)) / 1e-6
+
 def solve_equation(f):
     """
     求解方程 f = 0
     """
-    ######################
-    ### Your code here ###
-    ######################
-    return None
+
+    xi = 1
+    while abs(f(xi)) > 1e-6:
+        xi = xi - f(xi)/d(f)(xi)
+    return xi
 
 def section1_test(case):
     args = case[0] # (a, b)
@@ -99,47 +112,67 @@ def section3_test(case):
     assert(res == True)
 
 
-
 if __name__ == "__main__":
     print("正在检查 Section 1：")
-    assert(solve_linear(3, 6) == [-2])
-    assert(abs(solve_linear(3, 4)[0] + 4 / 3) < 1e-6)
-    assert(solve_linear(0, 5) == [])
-    assert(solve_linear(5, 0) == [0])
-    assert(solve_linear(0, 0) == None)
+    section1 = [ ((3, 6), [-2]),
+                 ((3, 4), [-4/3]),
+                 ((0, 5), []),
+                 ((5, 0), [0]),
+                 ((0, 0), None) ]
+    for case in section1:
+      section1_test(case)
+    # map(section1_test, section1)
     print("Section 1 正确")
 
     print("正在检查 Section 2：")
-    assert(solve_with_formula(1, 2, 1) == [-1])
-    assert(solve_with_formula(3, 2, 1) == [])
+    section2 = [ ([1, 2, 1], 1), 
+                 ([3, 2, 1], 0),
+                 ([4, 5, -6], 2),
+                 ([3, 5, 3], 0),
+                 ([-2, 4, 1], 2),
+                 ([0, 0, 5], 0),
+                 ([0, 5, 0], 1),
+                 ([0, 0, 0], -1) ]
 
-    sol = solve_with_formula(4, 5, -6)
-    # print(sol)
-    # assert(sol + 5 / 4 < 1e-6)
-    assert(abs(sol[0] + sol[1] + 5 / 4) < 1e-6)
-    assert(abs(sol[0] * sol[1] + 3 / 2) < 1e-6)
+    for case in section2:
+      section2_test(case)
 
-    sol = solve_with_formula(3, 5, 3)
-    assert(sol == [])
-    sol = solve_with_formula(-2, 4, 1)
-    assert(abs(sol[0] + sol[1] + 4 / -2) < 1e-6)
-    assert(abs(sol[0] * sol[1] + 1 / 2) < 1e-6)
+    # assert(solve_with_formula(1, 2, 1) == [-1])
+    # assert(solve_with_formula(3, 2, 1) == [])
 
-    assert(solve_with_formula(0, 0, 5) == [])
-    assert(solve_with_formula(0, 5, 0) == [0])
-    assert(solve_with_formula(0, 0, 0) == None)
+    # sol = solve_with_formula(4, 5, -6)
+    # # print(sol)
+    # # assert(sol + 5 / 4 < 1e-6)
+    # assert(abs(sol[0] + sol[1] + 5 / 4) < 1e-6)
+    # assert(abs(sol[0] * sol[1] + 3 / 2) < 1e-6)
+
+    # sol = solve_with_formula(3, 5, 3)
+    # assert(sol == [])
+    # sol = solve_with_formula(-2, 4, 1)
+    # assert(abs(sol[0] + sol[1] + 4 / -2) < 1e-6)
+    # assert(abs(sol[0] * sol[1] + 1 / 2) < 1e-6)
+
+    # assert(solve_with_formula(0, 0, 5) == [])
+    # assert(solve_with_formula(0, 5, 0) == [0])
+    # assert(solve_with_formula(0, 0, 0) == None)
     print("Section 2 正确")
 
     print("正在检查 Section 3：")
-    f = lambda x: math.log(x) + x
-    assert(abs(solve_equation(f) - 0.567143290) < 1e-6)
-    f = lambda x: x ** 3 - 7 * x ** 2 + 11 * x + 3
-    assert(
-        abs(solve_equation(f) - 3) < 1e-6 or
-        abs(solve_equation(f) - (2 - math.sqrt(5))) < 1e-6 or
-        abs(solve_equation(f) - (2 + math.sqrt(5))) < 1e-6
-        )
-    f = lambda x: -3 * (x ** 3) + (x ** 2) + 9
-    assert(abs(solve_equation(f) - 1.5623574989) < 1e-6)
-    print("Section 3 正确")
+    section3 = [ ('lgx+x', lambda x: math.log(x) + x, [0.567143290]),
+                 ('x^3-7x^2+11x+3', lambda x: x ** 3 - 7 * x ** 2 + 11 * x + 3, [3, (2 - math.sqrt(5)), (2 + math.sqrt(5))]),
+                 ('-3x^3+x^2+9', lambda x: -3 * (x ** 3) + (x ** 2) + 9, [1.5623574989]) ]
 
+    for case in section3:
+      section3_test(case)
+
+    # f = lambda x: math.log(x) + x
+    # assert(abs(solve_equation(f) - 0.567143290) < 1e-6)
+    # f = lambda x: x ** 3 - 7 * x ** 2 + 11 * x + 3
+    # assert(
+    #     abs(solve_equation(f) - 3) < 1e-6 or
+    #     abs(solve_equation(f) - (2 - math.sqrt(5))) < 1e-6 or
+    #     abs(solve_equation(f) - (2 + math.sqrt(5))) < 1e-6
+    #     )
+    # f = lambda x: -3 * (x ** 3) + (x ** 2) + 9
+    # assert(abs(solve_equation(f) - 1.5623574989) < 1e-6)
+    print("Section 3 正确")
